2cf5613e96ed4ca46a18159d71a72f5ec032cf1d
cyberGestalter <minto31415@mail.ru>
2021-07-23 06:44:58 +0300

    init commit

---
+++ app/src/main/java/com/example/and_work_3_additional/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.and_work_3_additional
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.os.AsyncTask
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.squareup.picasso.Picasso
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var inputLink: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.image_loaded)
+        inputLink = findViewById(R.id.input_link)
+    }
+
+    fun loadImageWithPicasso(view: View) {
+        imageView.setImageResource(R.drawable.before_load)
+        val imageLink = inputLink.text.toString()
+        if (imageLink.isEmpty()) {
+            setErrorImageAndToast()
+            return
+        }
+
+        val errorListener = Picasso.Listener { _, _, _ -> setErrorImageAndToast() }
+
+        Picasso.Builder(this)
+            .listener(errorListener)
+            .build()
+            .load(imageLink)
+            .error(R.drawable.failed_load)
+            .into(imageView)
+    }
+
+    fun loadImageWithGlide(view: View) {
+        imageView.setImageResource(R.drawable.before_load)
+        val imageLink = inputLink.text.toString()
+        if (imageLink.isEmpty()) {
+            setErrorImageAndToast()
+            return
+        }
+
+        val errorListener = object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?, model: Any?, target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                setErrorImageAndToast()
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?, model: Any?, target: Target<Drawable>?,
+                dataSource: DataSource?, isFirstResource: Boolean
+            ): Boolean {
+                return false
+            }
+        }
+
+        Glide.with(this)
+            .load(imageLink)
+            .error(R.drawable.failed_load)
+            .listener(errorListener)
+            .into(imageView)
+    }
+
+    fun loadImageWithAndroid(view: View) {
+        imageView.setImageResource(R.drawable.before_load)
+        val imageLink = inputLink.text.toString()
+        ImageLoader().execute(imageLink)
+    }
+
+    private fun setErrorImageAndToast() {
+        imageView.setImageResource(R.drawable.failed_load)
+        Toast
+            .makeText(this, getString(R.string.error_image_load_failed), Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    inner class ImageLoader : AsyncTask<String, Any, Any>() {
+        override fun doInBackground(vararg params: String?): Any? {
+            return try {
+                val link = params[0]
+                val imageURL = URL(link)
+                BitmapFactory.decodeStream(imageURL.openConnection().getInputStream())
+            } catch (e: Exception) {
+                null
+            }
+        }
+
+        override fun onPostExecute(result: Any?) {
+            if (result != null) {
+                imageView.setImageBitmap(result as Bitmap)
+            } else {
+                setErrorImageAndToast()
+            }
+        }
+
+    }
+}
